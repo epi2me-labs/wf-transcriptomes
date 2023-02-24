94a2e2c28c02ea413314b909d54b8f6ff197fb8a
Neil Horner <neil.horner@nanoporetech.com>
2023-02-24 16:49:28 +0000

    Update docs to remove JAFFAL install instructions

--- download_jaffal_references.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-#Download the data. We should we move the data out of Figshare?
-wget -O JAFFA_REFERENCE_FILES_HG38_GENCODE22.V2.tar.gz https://figshare.com/ndownloader/files/25410494
-tar -zxvf JAFFA_REFERENCE_FILES_HG38_GENCODE22.V2.tar.gz
