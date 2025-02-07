eab8ef1efeabbe82a35d17c4cf12387f633f1d56
Kiah McIntosh <kiah.mcintosh@nanoporetech.com>
2025-02-07 08:42:49 +0000

    Use args for R script inputs [CW-3398]

--- bin/de_analysis.R
+++ bin/de_analysis.R
@@ -1,22 +1,37 @@
 #!/usr/bin/env Rscript
 
+suppressMessages(library(argparser))
+
+parser <- arg_parser("Run differential expression analysis")
+parser <- add_argument(parser, "--annotation", help="Reference annotation.")
+parser <- add_argument(parser, "--min_samps_gene_expr", help="Minimum number of samples a gene must be expressed in to be included in differential gene expression.", type="numeric")
+parser <- add_argument(parser, "--min_samps_feature_expr", help="Minimum number of samples for differential transcript usage.", type="numeric")
+parser <- add_argument(parser, "--min_gene_expr", help="Minimum counts per gene required for differential gene expression.", type="numeric")
+parser <- add_argument(parser, "--min_feature_expr", help="Minimum counts per transcript required for differential transcript usage.", type="numeric")
+parser <- add_argument(parser, "--sample_sheet", help="Sample sheet.")
+parser <- add_argument(parser, "--all_counts", help="All transcript counts CSV file.")
+parser <- add_argument(parser, "--de_out_dir", help="Directory where differential expression out files will be saved. Directory will be created if it does not exist", default="de_analysis")
+parser <- add_argument(parser, "--merged_out_dir", help="Directory where merged count files will be saved. Directory will be created if it does not exist", default="merged")
+argv <- parse_args(parser)
+
 suppressMessages(library("DRIMSeq"))
 suppressMessages(library("GenomicFeatures"))
 suppressMessages(library("edgeR"))
-args <- commandArgs(trailingOnly=TRUE)
-ref_annotation <- args[1]
-min_samps_gene_expr <- as.numeric(args[2])
-min_samps_feature_expr <- as.numeric(args[3])
-min_gene_expr <- as.numeric(args[4]) 
-min_feature_expr <- as.numeric(args[5])
-sample_sheet <- args[6]
+
+# Create output directories
+if (!dir.exists(argv$de_out_dir)){
+  dir.create(argv$de_out_dir, recursive=TRUE)
+}
+if (!dir.exists(argv$merged_out_dir)){
+  dir.create(argv$merged_out_dir, recursive=TRUE)
+}
 
 cat("Loading counts, conditions and parameters.\n")
-cts <- as.matrix(read.csv("all_counts.tsv", sep="\t", row.names="Reference", stringsAsFactors=FALSE))
+cts <- as.matrix(read.csv(argv$all_counts, sep="\t", row.names="Reference", stringsAsFactors=FALSE))
 
 # Set up sample data frame:
 #changed this to sample_id
-coldata <- read.csv(sample_sheet, row.names="alias", sep=",", stringsAsFactors=TRUE)
+coldata <- read.csv(argv$sample_sheet, row.names="alias", sep=",", stringsAsFactors=TRUE)
 
 coldata$sample_id <- rownames(coldata)
 # check if control condition exists, sets as reference 
@@ -30,7 +45,7 @@ coldata$condition <- relevel(coldata$condition, ref = "control")
 # see https://www.ensembl.org/info/website/upload/gff.html
 # and http://gmod.org/wiki/GFF2#Converting_GFF2_to_GFF3
 cat("Checking annotation file type.\n")
-lines <- readLines(file(ref_annotation), n=10000)
+lines <- readLines(file(argv$annotation), n=10000)
 # If transcript_id containing '=' (format eg. transcript_id=xxx)
 # annotation type is gff3
 check_file_type <- sum(grepl("transcript_id=", lines))
@@ -49,7 +64,7 @@ if (check_file_type != 0){
 # The following handles this.
 cat("Checking annotation file for presence of transcript_id versions.\n")
 # Get the first transcript_id from the annotation file by parsing
-lines <- readLines(file(ref_annotation), n=100000)
+lines <- readLines(file(argv$annotation), n=100000)
 # Find transcript_ids in first 1000 lines and check if they contain dot (format eg. ENTXXX.1)
 check_version <- sum(grepl("transcript_id[^;]+\\.", lines))
 if (check_version != 0){
@@ -62,7 +77,7 @@ if (check_version != 0){
     }
 
 cat("Loading annotation database.\n")
-txdb <- makeTxDbFromGFF(ref_annotation,  format = annotation_type)
+txdb <- makeTxDbFromGFF(argv$annotation,  format = annotation_type)
 txdf <- select(txdb, keys(txdb,"GENEID"), "TXNAME", "GENEID")
 tab <- table(txdf$GENEID)
 txdf$ntx<- tab[match(txdf$GENEID, names(tab))]
@@ -78,15 +93,15 @@ rownames(txdf) <- NULL
 counts<-data.frame(gene_id=txdf$GENEID, feature_id=txdf$TXNAME, cts)
 
 # output unfiltered version of the counts table now we have paired transcripts with gene ids
-write.table(counts, file="de_analysis/unfiltered_transcript_counts_with_genes.tsv", sep="\t", row.names = FALSE, quote=FALSE)
+write.table(counts, file=file.path(argv$de_out_dir, "unfiltered_transcript_counts_with_genes.tsv"), sep="\t", row.names = FALSE, quote=FALSE)
 
 cat("Filtering counts using DRIMSeq.\n")
 
 d <- dmDSdata(counts=counts, samples=coldata)
 trs_cts_unfiltered <- counts(d)
 
-d <- dmFilter(d, min_samps_gene_expr = min_samps_gene_expr, min_samps_feature_expr = min_samps_feature_expr,
-        min_gene_expr = min_gene_expr, min_feature_expr = min_feature_expr)
+d <- dmFilter(d, min_samps_gene_expr=argv$min_samps_gene_expr, min_samps_feature_expr=argv$min_samps_feature_expr,
+        min_gene_expr=argv$min_gene_expr, min_feature_expr=argv$min_feature_expr)
 
 cat("Building model matrix.\n")
 design <- model.matrix(~condition, data=DRIMSeq::samples(d))
@@ -98,12 +113,12 @@ suppressMessages(library("dplyr"))
 # Sum transcript counts into gene counts:
 cat("Sum transcript counts into gene counts.\n")
 trs_cts <- counts(d)
-write.table(trs_cts, file="merged/filtered_transcript_counts_with_genes.tsv", sep="\t", row.names = FALSE, quote=FALSE)
+write.table(trs_cts, file=file.path(argv$merged_out_dir, "filtered_transcript_counts_with_genes.tsv"), sep="\t", row.names = FALSE, quote=FALSE)
 
 gene_cts <- trs_cts_unfiltered %>% dplyr::select(c(1, 3:ncol(trs_cts)))  %>% group_by(gene_id) %>% summarise_all(tibble::lst(sum)) %>% data.frame()
 rownames(gene_cts) <- gene_cts$gene_id
 gene_cts$gene_id <- NULL
-write.table(gene_cts, file="merged/all_gene_counts.tsv", sep="\t", quote=FALSE)
+write.table(gene_cts, file=file.path(argv$merged_out_dir, "all_gene_counts.tsv"), sep="\t", quote=FALSE)
 
 # Output count per million of the gene counts using edgeR CPM
 cpm_gene_counts <- cpm(gene_cts)
@@ -111,7 +126,7 @@ cpm_gene_counts <- cpm(gene_cts)
 cpm_gene_counts <- cbind(var_name = rownames(cpm_gene_counts), cpm_gene_counts)
 rownames(cpm_gene_counts) <- NULL
 colnames(cpm_gene_counts)[1] <- "gene_id"
-write.table(cpm_gene_counts, file="de_analysis/cpm_gene_counts.tsv", sep="\t", quote=FALSE, row.names = FALSE)
+write.table(cpm_gene_counts, file=file.path(argv$de_out_dir, "cpm_gene_counts.tsv"), sep="\t", quote=FALSE, row.names = FALSE)
 
 # Differential gene expression using edgeR:
 cat("Running differential gene expression analysis using edgeR.\n")
@@ -139,7 +154,7 @@ plotMD(qlf, status=status,  values=c("up","down","notsig"), hl.col=c("red","blue
 abline(h=c(-1,1), col="blue")
 plotQLDisp(fit)
 
-write.table(as.data.frame(edger_res), file="de_analysis/results_dge.tsv", sep="\t")
+write.table(as.data.frame(edger_res), file=file.path(argv$de_out_dir, "results_dge.tsv"), sep="\t")
 
 # Differential transcript usage using DEXSeq:
 suppressMessages(library("DEXSeq"))
@@ -166,8 +181,8 @@ dxr.g <- dxr.g[order(dxr.g$qval),]
 dxr_out <- as.data.frame(dxr[,c("featureID", "groupID", "pvalue")])
 dxr_out <- dxr_out[order(dxr$pvalue),]
 
-write.table(dxr.g, file="de_analysis/results_dtu_gene.tsv", sep="\t")
-write.table(dxr_out, file="de_analysis/results_dtu_transcript.tsv", sep="\t")
+write.table(dxr.g, file=file.path(argv$de_out_dir, "results_dtu_gene.tsv"), sep="\t")
+write.table(dxr_out, file=file.path(argv$de_out_dir, "results_dtu_transcript.tsv"), sep="\t")
 
 # and writing out some of the DEXSeq metrics to accompany EPI2ME Labs tutorial
 colnames(dxr)[grep("log2fold", colnames(dxr))] <- "log2fold"
@@ -175,7 +190,7 @@ MADTUdata <- data.frame(dxr)[order(dxr$padj),c("exonBaseMean", "log2fold", "pval
 MADTUdata$exonBaseMean <- log2(MADTUdata$exonBaseMean)
 colnames(MADTUdata)[which(colnames(MADTUdata)=="exonBaseMean")] <- "Log2MeanExon"
 colnames(MADTUdata)[which(colnames(MADTUdata)=="log2fold")] <- "Log2FC"
-write.table(MADTUdata, file="de_analysis/results_dexseq.tsv", sep="\t")
+write.table(MADTUdata, file=file.path(argv$de_out_dir, "results_dexseq.tsv"), sep="\t")
 
 # stageR analysis of DEXSeq results:
 cat("stageR analysis\n")
@@ -194,4 +209,4 @@ stageRObj <- stageWiseAdjustment(stageRObj, method="dtu", alpha=0.10)
 suppressWarnings({dex.padj <- getAdjustedPValues(stageRObj, order=FALSE, onlySignificantGenes=FALSE)})
 
 # dex.padj <- dex.padj[,-1]
-write.table(dex.padj, file="de_analysis/results_dtu_stageR.tsv", sep="\t")
+write.table(dex.padj, file=file.path(argv$de_out_dir, "results_dtu_stageR.tsv"), sep="\t")
